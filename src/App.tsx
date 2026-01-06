1bda730be76034924e41bf6ef2dc85aacfa5168b
Xaibra <36691057+Xaibra@users.noreply.github.com>
2026-01-06 16:19:26 +0300

    Add Experts page import and /experts route

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,24 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import AdminLogin from "./pages/AdminLogin";
-import AdminDashboard from "./pages/AdminDashboard";
-import AdminTeam from "./pages/AdminTeam";
-import AdminNews from "./pages/AdminNews";
-import AdminLayout from "@/components/admin/AdminLayout";
-import ErrorBoundary from "@/components/ErrorBoundary";
-import { AdminProvider } from "@/contexts/AdminContext";
-import { DataProvider } from "@/contexts/DataContext";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Experts from './pages/Experts';
+import Footer from './components/Footer';
+import './App.css';
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <ErrorBoundary>
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <DataProvider>
-          <AdminProvider>
-            <Toaster />
-            <Sonner />
-            <BrowserRouter>
-              <Routes>
-                {/* Public Routes */}
-                <Route path="/" element={<Index />} />
-                
-                {/* Admin Routes */}
-                <Route path="/admin/login" element={<AdminLogin />} />
-                <Route path="/admin" element={<AdminLayout />}>
-                  <Route path="dashboard" element={<AdminDashboard />} />
-                  <Route path="team" element={<AdminTeam />} />
-                  <Route path="news" element={<AdminNews />} />
-                  <Route index element={<AdminDashboard />} />
-                </Route>
-                
-                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </BrowserRouter>
-          </AdminProvider>
-        </DataProvider>
-      </TooltipProvider>
-    </QueryClientProvider>
-  </ErrorBoundary>
-);
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/experts" element={<Experts />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
 
 export default App;
